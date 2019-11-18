cc1da50866277a1a783285ed1cf7ce90830225a3
lata-clabs <lata@cyberlabsindia.com>
2019-11-18 15:45:30 +0530

    Updates

---
+++ precache-manifest.89d19190ab6441478e9713b2a59edb12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed2da796d70b96cde3dcb04592b31c1",
+    "url": "/barcode-react-version/index.html"
+  },
+  {
+    "revision": "edb7c3fb582aca89c5ee",
+    "url": "/barcode-react-version/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "986e80937a2f5f14573d",
+    "url": "/barcode-react-version/static/css/main.eb1ecc82.chunk.css"
+  },
+  {
+    "revision": "edb7c3fb582aca89c5ee",
+    "url": "/barcode-react-version/static/js/2.a9c0658e.chunk.js"
+  },
+  {
+    "revision": "986e80937a2f5f14573d",
+    "url": "/barcode-react-version/static/js/main.25533241.chunk.js"
+  },
+  {
+    "revision": "79a165a326bf131e49eb",
+    "url": "/barcode-react-version/static/js/runtime-main.854b9e53.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/barcode-react-version/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4c8e690065db5f525986e0489ddc455a",
+    "url": "/barcode-react-version/static/media/user.4c8e6900.png"
+  }
+]);
